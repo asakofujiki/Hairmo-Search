5630f02da0b412c5331946156a70b76fe0a87795
asakofujiki <a.fujiki1554@gmail.com>
2020-07-09 16:16:45 +0000

    deploy setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,5 +60,5 @@
 #     # password: "please use keys"
 #   }
 
-server '3.112.213.155', user: 'ec2-user', roles: %w{app web db}
-server '18.181.187.49', user: 'ec2-user', roles: %w{app web db}
+#server '3.112.213.155', user: 'ec2-user', roles: %w{app web db}
+#server '18.181.187.49', user: 'ec2-user', roles: %w{app web db}
